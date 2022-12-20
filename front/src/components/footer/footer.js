62d7daabea371737dce76cf3fbe66e0a6346b1ad
Adrien <103190086+Adr-hub@users.noreply.github.com>
2022-12-20 19:50:06 +0100

    Footer layout update
    Links were added for the bilingual portfolio feature.

--- front/src/components/footer/footer.js
+++ front/src/components/footer/footer.js
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom';
 import '../../styles/components/footer/footer.scss';
 const Footer = () => {
     return <footer><p>Année de production : 2022</p>
         <div className="external-links"><div className="first-external-link"><a href="https://www.linkedin.com/in/adrien-rival-1b6a74256/" rel="noreferrer" target="_blank"><i className="fab fa-linkedin"></i></a></div>
             <div className="second-external-link"><a href="https://github.com/Adr-hub" rel="noreferrer" target="_blank"><i className="fab fa-github"></i></a></div>
-        </div></footer>
+        </div><div className='languages'><Link className='fr'>FR /</Link><Link className='en'>EN</Link></div></footer>
 };
 export default Footer;
