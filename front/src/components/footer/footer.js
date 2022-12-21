28397a6d0ca943d663cb8559198c1ce7512d4bb7
Adrien <103190086+Adr-hub@users.noreply.github.com>
2022-12-21 01:10:06 +0100

    Adjustments
    Added footer links were renamed.

--- front/src/components/footer/footer.js
+++ front/src/components/footer/footer.js
@@ -4,6 +4,6 @@ const Footer = () => {
     return <footer><p>Année de production : 2022</p>
         <div className="external-links"><div className="first-external-link"><a href="https://www.linkedin.com/in/adrien-rival-1b6a74256/" rel="noreferrer" target="_blank"><i className="fab fa-linkedin"></i></a></div>
             <div className="second-external-link"><a href="https://github.com/Adr-hub" rel="noreferrer" target="_blank"><i className="fab fa-github"></i></a></div>
-        </div><div className='languages'><Link className='fr'>FR /</Link><Link className='en'>EN</Link></div></footer>
+        </div><div className='languages'><Link className='fr'>Français /</Link><Link className='en'>English</Link></div></footer>
 };
 export default Footer;
